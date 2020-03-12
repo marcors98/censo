69b069b886b4faa2d631aadf9b4626a54781ec75
Alumno <ulsa@3.local>
2020-03-11 17:22:23 -0700

    otro commit

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:api')->get('/usuario','UsuarioApiController@usuario');
+
+Route::middleware('auth:api')->post('/suma','UsuarioApiController@suma');
+
+Route::apiResource('casas','CasasApiController');
